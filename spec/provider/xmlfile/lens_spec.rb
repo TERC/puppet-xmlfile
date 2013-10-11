331ed7836615af0b933ce0a8d3de291f0e424472
cammoraton <nick_cammorato@terc.edu>
2013-10-10 21:27:51 -0400

    Skeletoned out lens spec a bit more.

--- spec/provider/xmlfile/lens_spec.rb
+++ spec/provider/xmlfile/lens_spec.rb
@@ -1,7 +1,103 @@
 require 'spec_helper'
 require 'puppet/provider/xmlfile/lens'
+require 'puppet/util/diff'
 
 describe XmlLens do  
   let(:testobject) { XmlLens }
   # Build out tests as we come up with comparisons to augeas
+  before(:all) do
+    # Initialize our content as the default activemq xml file as it
+    # has most of the appropriate xml warts(and is why we wrote this).
+    @content = <<-'EOT'
+<beans
+  xmlns="http://www.springframework.org/schema/beans"
+  xmlns:amq="http://activemq.apache.org/schema/core"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd
+  http://activemq.apache.org/schema/core http://activemq.apache.org/schema/core/activemq-core.xsd">
+
+    <bean class="org.springframework.beans.factory.config.PropertyPlaceholderConfigurer">
+        <property name="locations">
+            <value>file:${activemq.conf}/credentials.properties</value>
+        </property>
+    </bean>
+
+    <broker xmlns="http://activemq.apache.org/schema/core" brokerName="localhost" dataDirectory="${activemq.data}">
+ 
+        <destinationPolicy>
+            <policyMap>
+              <policyEntries>
+                <policyEntry topic=">" producerFlowControl="true">
+                  <pendingMessageLimitStrategy>
+                    <constantPendingMessageLimitStrategy limit="1000"/>
+                  </pendingMessageLimitStrategy>
+                </policyEntry>
+                <policyEntry queue=">" producerFlowControl="true" memoryLimit="1mb">
+                </policyEntry>
+              </policyEntries>
+            </policyMap>
+        </destinationPolicy>
+
+        <managementContext>
+            <managementContext createConnector="false"/>
+        </managementContext>
+ 
+        <persistenceAdapter>
+            <kahaDB directory="${activemq.data}/kahadb"/>
+        </persistenceAdapter>
+
+          <systemUsage>
+            <systemUsage>
+                <memoryUsage>
+                    <memoryUsage limit="64 mb"/>
+                </memoryUsage>
+                <storeUsage>
+                    <storeUsage limit="100 gb"/>
+                </storeUsage>
+                <tempUsage>
+                    <tempUsage limit="50 gb"/>
+                </tempUsage>
+            </systemUsage>
+        </systemUsage>
+ 
+        <transportConnectors>
+            <!-- DOS protection, limit concurrent connections to 1000 and frame size to 100MB -->
+            <transportConnector name="openwire" uri="tcp://0.0.0.0:61616?maximumConnections=1000&amp;wireFormat.maxFrameSize=104857600"/>
+            <transportConnector name="amqp" uri="amqp://0.0.0.0:5672?maximumConnections=1000&amp;wireFormat.maxFrameSize=104857600"/>
+        </transportConnectors>
+
+        <shutdownHooks>
+            <bean xmlns="http://www.springframework.org/schema/beans" class="org.apache.activemq.hooks.SpringContextHook" />
+        </shutdownHooks>
+ 
+    </broker>
+
+    <import resource="jetty.xml"/>
+ 
+</beans>
+    EOT
+    @rexml = REXML::Document.new(@content)
+    
+    # Create and initialize the tempfile
+    file = File.open("/tmp/xmllens_test.tmp", "w+")
+    @rexml.write(file)
+    file.close
+  end
+  
+  after(:all) do
+    # Delete the tempfile
+    File.unlink("/tmp/xmllens_test.tmp")
+  end
+
+  describe :add do
+    it "should create identical output to augeas"
+  end
+  
+  describe :set do
+    it "should create identical output to augeas"
+  end
+  
+  describe :rm do
+    it "should create identical output to augeas"
+  end
 end
